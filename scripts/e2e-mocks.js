dcbf9b2e5cdd34d4f8010698bc8ed8b7a8970465
torben <t.hansen@skyfillers.com>
2014-03-07 08:25:11 +0100

    [TASK] mock JSON file  - Refs #10

--- scripts/e2e-mocks.js
+++ scripts/e2e-mocks.js
@@ -3,7 +3,9 @@
 angular.module('e2e-mocks', ['ngMockE2E'])
     .run(function($httpBackend) {
 
-        $httpBackend.whenGET('data/quixr.json').respond({'vhost1':{'traffic':{'2014':{'1':{'1':1024000}}}}});
+        var response = {'vhost1':{'traffic':{'2014':{'1':{'1':1024},'2':{'1':1048576},'3':{'1':1073741824},'4':{'1':1099511600000},'5':{'1':1024,'2':2048,'3':4096}}},'diskspace':{'2014':{'1':{'1':1024},'2':{'1':1048576},'3':{'1':1073741824},'4':{'1':1099511600000},'5':{'1':1024,'2':2048,'3':4096}}}}, 'vhost2':{'traffic':{'2014':{'1':{'1':1024},'2':{'1':1048576},'3':{'1':1073741824},'4':{'1':1099511600000}}},'diskspace':{'2014':{'1':{'1':1024},'2':{'1':1048576},'3':{'1':1073741824},'4':{'1':1099511600000}}}}};
+
+        $httpBackend.whenGET('data/quixr.json').respond(response);
 
         // Don't mock the html views
         $httpBackend.whenGET(/views\/\w+.*/).passThrough();
@@ -11,6 +13,7 @@ angular.module('e2e-mocks', ['ngMockE2E'])
         // For everything else, don't mock
         $httpBackend.whenGET(/^\w+.*/).passThrough();
         $httpBackend.whenPOST(/^\w+.*/).passThrough();
+
 });
 
 angular.module('quixrWebview').requires.push('e2e-mocks');
